758651abe64cae1468ae9a23dfd46b50d5222db2
eunsukimme <eunsu.dev@gmail.com>
2022-10-10 14:18:42 +0900

    Initial commit

---
+++ test/aws-communityday-2022.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCommunityday2022 from '../lib/aws-communityday-2022-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-communityday-2022-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCommunityday2022.AwsCommunityday2022Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
