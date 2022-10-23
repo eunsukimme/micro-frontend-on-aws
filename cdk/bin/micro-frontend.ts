6f22a57cc6c770720eb97149b3b25b72ba24b2c3
eunsukimme <eunsu.dev@gmail.com>
2022-10-23 18:45:07 +0900

    infra: remove account id from stack env

--- cdk/bin/micro-frontend.ts
+++ cdk/bin/micro-frontend.ts
@@ -4,8 +4,6 @@ import { MicroFrontendStack } from "../lib/micro-frontend";
 const app = new cdk.App();
 new MicroFrontendStack(app, "MicroFrontendStack", {
   env: {
-    region: 'ap-northeast-2',
-    account: '436874380673',
+    region: "ap-northeast-2",
   },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
